39fce9e591af011e2be4d4e9c2f8c3a2055ef9c1
Palle <palleklewitz@icloud.com>
2017-04-29 16:48:00 +0200

    Initial Commit

---
+++ SOMRenderer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SOMRenderer
+//
+//  Created by Palle Klewitz on 29.04.17.
+//  Copyright © 2017 Palle Klewitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
